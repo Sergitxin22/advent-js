72902bffcda9c45adcb6b211e08344038b146c8c
Sergitxin22 <sergio.morales@opendeusto.es>
2024-12-08 19:26:06 +0100

    Upload Reto 1 solutions

---
+++ Reto #1 - 🎁 ¡Primer regalo repetido!/script.ts
@@ -0,0 +1,17 @@
+function prepareGifts(gifts: number[]): number[] {
+    const valoresUnicos: Set<number> = new Set(gifts)
+    const valoresOrdenados: number[] = [...valoresUnicos].sort((a, b) => a - b)
+    return valoresOrdenados
+}
+
+const gifts1: number[] = [3, 1, 2, 3, 4, 2, 5]
+const preparedGifts1: number[] = prepareGifts(gifts1)
+console.log(preparedGifts1) // [1, 2, 3, 4, 5]
+
+const gifts2: number[] = [6, 5, 5, 5, 5]
+const preparedGifts2: number[] = prepareGifts(gifts2)
+console.log(preparedGifts2) // [5, 6]
+
+const gifts3: number[] = []
+const preparedGifts3: number[] = prepareGifts(gifts3)
+console.log(preparedGifts3) // [] No hay regalos, la lista queda vacía
